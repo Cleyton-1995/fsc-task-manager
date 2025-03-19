5f45176f2d9106c74d4ccf1676468c587d26afab
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-03-19 19:15:04 -0300

    fix: resolve conflicts between Prettier and Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: ['Poppins', 'sans-serif'],
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // ✅ Correto
+      },
+    },
   },
   plugins: [],
 };
