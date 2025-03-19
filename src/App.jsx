a3902b96224600a8e2839ae6ebb7bbd064d839f3
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-03-19 14:43:00 -0300

    chore: add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { Tasks } from './components/Tasks';
 
 function App() {
-  return <Tasks />;
+  return (
+    <h1 className="text-red-500">
+      <Tasks />
+      <p>abc</p>
+    </h1>
+  );
 }
 
 export default App;
