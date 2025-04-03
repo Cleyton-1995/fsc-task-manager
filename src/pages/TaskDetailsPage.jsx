44fad320a7932b85df6b7616a31b317db4904bec
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-04-03 15:38:55 -0300

    feat: ensure task can be deleted

--- src/pages/TaskDetailsPage.jsx
+++ src/pages/TaskDetailsPage.jsx
@@ -93,6 +93,21 @@ export default function TaskDetailsPage() {
     toast.success('Tarefa salva com sucesso!');
   }
 
+  async function handleDeleteClick() {
+    const response = await fetch(`http://localhost:3000/tasks/${task.id}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      return toast.error(
+        'Erro ao deletar a tarefa. Por favor, tente novamente'
+      );
+    }
+
+    toast.success('Tarefa deletada com sucesso!');
+    navigate(-1);
+  }
+
   const titleError = errors.find((error) => error.inputName === 'title');
   const timeError = errors.find((error) => error.inputName === 'time');
   const descriptionError = errors.find(
@@ -126,7 +141,11 @@ export default function TaskDetailsPage() {
             <h1 className="mt-2 text-xl font-semibold">{task?.title}</h1>
           </div>
 
-          <Button className="h-fit self-end" color="danger">
+          <Button
+            className="h-fit self-end"
+            color="danger"
+            onClick={handleDeleteClick}
+          >
             {' '}
             <TrashIcon /> Deletar Tarefa
           </Button>
