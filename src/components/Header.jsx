9b749d057ab0dcb5834c77a6bc57ba98d451ab40
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-04-15 15:02:28 -0300

    feat: add handleDeleteAllTasks function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { toast } from 'sonner';
 
 import { AddIcon, TrashIcon } from '../assets/icons';
+import useDeleteAllTasks from '../hooks/data/useDeleteAllTasks';
 import AddTasksDialog from './AddTasksDialog';
 import Button from './Button';
 
 export default function Header({ subtitle, title }) {
   const [addTasksDialogIsOpen, setAddTasksDialogIsOpen] = useState(false);
+
+  const { mutate: deleteAllTasks, isPending } = useDeleteAllTasks();
+
+  function handleDeleteAllTasks() {
+    deleteAllTasks(undefined, {
+      onSuccess: () => toast.success('Todas as tarefas foram removidas!'),
+      onError: () => toast.error('Erro ao limpar as tarefas.'),
+    });
+  }
+
   return (
     <div className="flex w-full justify-between">
       <div>
@@ -17,7 +29,11 @@ export default function Header({ subtitle, title }) {
       </div>
 
       <div className="flex items-center gap-3">
-        <Button color="ghost">
+        <Button
+          onClick={handleDeleteAllTasks}
+          disabled={isPending}
+          color="ghost"
+        >
           Limpar Tarefas
           <TrashIcon />
         </Button>
